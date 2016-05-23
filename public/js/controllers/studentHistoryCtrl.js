b6150a0fa7ed99147faa6d2297740e561eb0d126
jamesoncodes <jamesoncodes@gmail.com>
2016-05-23 11:46:03 -0400

    all code is refactored and functioning

--- public/js/controllers/studentHistoryCtrl.js
+++ public/js/controllers/studentHistoryCtrl.js
@@ -5,11 +5,11 @@ angular.module('attendanceApp')
     $scope.studentId = $stateParams.student;
     console.log($scope.studentId);
     $scope.studentData =[];
-
+    //use promise. wait for data to return then run
     studentData.getClassData().then(function(students){
       $scope.students = students;
       console.log($scope.students);
-
+      //find studentId match
       for(var i = 0; i <= students.length; i++){
           if(students[i].studentId == $scope.studentId)
             $scope.studentData.push(students[i]);
