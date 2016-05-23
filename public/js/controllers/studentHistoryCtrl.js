899f30f6dfa9954e10e513e73f20338658d21595
jamesoncodes <jamesoncodes@gmail.com>
2016-05-23 09:07:42 -0400

    quick workaround for faulty api.can now display individual student info

--- public/js/controllers/studentHistoryCtrl.js
+++ public/js/controllers/studentHistoryCtrl.js
@@ -4,18 +4,19 @@ angular.module("attendanceApp")
     console.log($scope.studentId); //grabbed from param
 
     //store student data
-    $scope.studentData =[]; //API IS NOT WORKING. TESTED WITH POSTMAN, RETURNING AN EMPTY ARRAY
+    $scope.studentData =[]; //API IS NOT WORKING. TESTED WITH POSTMAN, RETURNING AN EMPTY ARRAY. 
+    //Plan B loop thru all data a second time.
 
-    //get all student data - for a specific student
+    //get all student data and match studentId for a specific student
      $http({
           method: 'GET',
-          url: 'https://script.google.com/macros/s/AKfycbxtbtw8aL1oY3eyxjFE98kOCqdaG4T4pWvPbNcEv9PlWHPbHQF_/exec?studentId='+$scope.studentId,
+          url: 'https://script.googleusercontent.com/macros/echo?user_content_key=q3CYF2thTPqe4a0A-0-UhF-Slp3ZHPnvx0lTeO28OV1R4WFQ0eFbeA5tHX_1GlppCxQ-rY62G_UOcLfO7mfGug4L1DQRPsCsm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnNYYfJlGdgHzYJZQwCxCgTje5cfV7yw9E3HqIKO543MazTAG79LdvSx5j0fAD_Z8_9pytj_A4aoE&lib=M4eROh2CW3-Nf8izyy0Rlae3i8jI5dE-p',
        }).success(function(data){
-          for(var i = 0; i < data.length; i++){
-            $scope.studentData.push(data[i]);
+          for(var i = 0; i <= data.length; i++){
+            if(data[i].studentId == $scope.studentId)
+              $scope.studentData.push(data[i]);
           }
       }).error(function(){
           alert("error");
       });
-      console.log($scope.studentData); //empty
 });
