7e0a7dcb394f9145727499f5d05407f5d521eedd
jamesoncodes <jamesoncodes@gmail.com>
2016-05-23 21:34:08 -0400

    final code review before interview. modualr. maintainable. fully functioning

--- public/js/controllers/studentHistoryCtrl.js
+++ public/js/controllers/studentHistoryCtrl.js
@@ -1,14 +1,17 @@
 angular.module('attendanceApp')
   .controller('studentHistoryCtrl', function($scope, $stateParams, studentData) {
+
     //get all  data
     $scope.students;
     $scope.studentId = $stateParams.student;
     console.log($scope.studentId);
     $scope.studentData =[];
+
     //use promise. wait for data to return then run
     studentData.getClassData().then(function(students){
       $scope.students = students;
-      console.log($scope.students);
+      //console.log($scope.students);
+
       //find studentId match
       for(var i = 0; i <= students.length; i++){
           if(students[i].studentId == $scope.studentId)
--- public/js/services/studentDataService.js
+++ public/js/services/studentDataService.js
@@ -1,18 +1,21 @@
 angular.module('attendanceApp')
     .factory('studentData', function($http, $q){
 
+  // need this service to handle the data then notify the controller when it finish
+
   return {
     getClassData: function() {
       var deferred = $q.defer(),
       httpPromise = $http.get('https://script.googleusercontent.com/macros/echo?user_content_key=q3CYF2thTPqe4a0A-0-UhF-Slp3ZHPnvx0lTeO28OV1R4WFQ0eFbeA5tHX_1GlppCxQ-rY62G_UOcLfO7mfGug4L1DQRPsCsm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnNYYfJlGdgHzYJZQwCxCgTje5cfV7yw9E3HqIKO543MazTAG79LdvSx5j0fAD_Z8_9pytj_A4aoE&lib=M4eROh2CW3-Nf8izyy0Rlae3i8jI5dE-p');
 
       httpPromise.success(function(students) {
-      deferred.resolve(students);
-    })
-    .error(function(error) {
-      console.log('something went wrong');
+        deferred.resolve(students);
+      })
+      .error(function(error) {
+        deferred.reject();
+        console.log(error);
     });
-    return deferred.promise;
+      return deferred.promise;
     }
   };
 });
